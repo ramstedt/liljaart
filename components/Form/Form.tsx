2f64d60b1a816ce07bddb10b5c331aafa8867d50
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2025-10-16 17:33:49 +0200

    update favicon and form.tsx

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState, FormEvent } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import styles from './Form.module.css';
+import CtaButton from '../CtaButton/CtaButton';
 
 export default function Form() {
   const recaptchaRef = useRef<ReCAPTCHA>(null);
@@ -55,40 +56,39 @@ export default function Form() {
       setReferrer('');
       setMessage('');
       resetCaptcha();
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        setError(err.message);
-      } else {
-        setError('An error occurred.');
-      }
+    } catch (err: any) {
+      setError(err.message || 'An error occurred.');
       resetCaptcha();
     }
   }
 
   return (
     <section className={styles.formSection}>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <input
-          type='text'
-          placeholder='Namn'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <input
-          type='email'
-          placeholder='Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type='text'
-          placeholder='Hur hittade du mig?'
-          value={referrer}
-          onChange={(e) => setReferrer(e.target.value)}
-        />
+      <form onSubmit={handleSubmit} className={styles.contactForm}>
+        <div className={styles.inputFields}>
+          <input
+            type='text'
+            placeholder='Namn'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            type='email'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type='text'
+            placeholder='Hur hittade du mig?'
+            value={referrer}
+            onChange={(e) => setReferrer(e.target.value)}
+          />
+        </div>
         <textarea
+          name='message'
           placeholder='Meddelande'
           value={message}
           onChange={(e) => setMessage(e.target.value)}
@@ -99,9 +99,9 @@ export default function Form() {
           sitekey={siteKey}
           onChange={(val) => setCaptchaToken(val)}
         />
+        <CtaButton type='submit'>Skicka</CtaButton>
         {error && <p className={styles.error}>{error}</p>}
         {success && <p className={styles.success}>Meddelandet skickades!</p>}
-        <button type='submit'>Skicka</button>
       </form>
     </section>
   );
